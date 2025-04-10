6e03e63c2cec29b30145fb6058cbf83cdc9fc452
Hasmukh Kumar <hasmukh+zombierobo@users.noreply.github.com>
2025-04-10 20:05:32 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hi, This is Hasmukh</h1>
+      <p className="read-the-docs">Under construction personal website.</p>
+    </>
+  );
+}
+
+export default App;
